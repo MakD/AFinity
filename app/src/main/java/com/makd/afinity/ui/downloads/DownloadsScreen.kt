b9a88b0c505e5825c2d833886d0fc97e7993779c
MakD <mainak999@gmail.com>
2025-10-20 22:33:26 +0530

    feat(downloads): Implement basic Downloads screen UI
    This commit introduces the basic user interface structure for the "Downloads" screen.
    
    A `Scaffold` has been implemented which includes a top app bar, consistent with other main screens in the application. The top bar features the title "Downloads", along with icons for navigating to the Search and Settings screens.
    
    The main content area of the screen is currently an empty `Box` that will be populated with download-related content in future commits.

--- app/src/main/java/com/makd/afinity/ui/downloads/DownloadsScreen.kt
+++ app/src/main/java/com/makd/afinity/ui/downloads/DownloadsScreen.kt
@@ -1,9 +1,19 @@
 package com.makd.afinity.ui.downloads
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import com.makd.afinity.data.models.media.AfinityItem
+import com.makd.afinity.navigation.Destination
+import com.makd.afinity.ui.components.AfinityTopAppBar
 import com.makd.afinity.ui.main.MainUiState
 
 @Composable
@@ -15,4 +25,35 @@ fun DownloadsScreen(
     navController: NavController
 ) {
     // TODO: Implement the UI for the Downloads screen, similar to FavoritesScreen
+    Scaffold(
+        topBar = {
+            AfinityTopAppBar(
+                title = {
+                    Text(
+                        text = "Downloads",
+                        style = MaterialTheme.typography.headlineLarge.copy(
+                            fontWeight = FontWeight.Bold
+                        ),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                },
+                onSearchClick = {
+                    val route = Destination.createSearchRoute()
+                    navController.navigate(route)
+                },
+                onProfileClick = {
+                    val route = Destination.createSettingsRoute()
+                    navController.navigate(route)
+                },
+                userProfileImageUrl = mainUiState.userProfileImageUrl
+            )
+        },
+        modifier = modifier
+    ) {paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(MaterialTheme.colorScheme.background)
+        )}
 }
